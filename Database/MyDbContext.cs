e39802597baa3bd35b29ef04c71165b612f313cb
Zaw Htut Aung <zawhtut0010zha@gmail.com>
2024-02-07 15:52:36 +0630

    Add project files.

---
+++ Database/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ZawHtutTest.Models;
+
+namespace ZawHtutTest.Database
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options ) : base(options)
+        {
+                
+        }
+
+        public virtual DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZawHtutTest.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public Guid UniqueKey { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
