a87424cb55650d412ddca4ff55820111733a868e
Zaw Htut Aung <zawhtut0010zha@gmail.com>
2024-02-08 16:08:07 +0630

    change some features

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,4 +1,8 @@
+
 using Microsoft.AspNetCore.Mvc;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
 
 namespace ZawHtutTest.Controllers
 {
@@ -10,6 +14,7 @@ namespace ZawHtutTest.Controllers
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
+
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -29,5 +34,55 @@ namespace ZawHtutTest.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet("GenerateFile")]
+        public IActionResult GeneratePDF()
+        {
+            return Ok();
+        }
+
+        [HttpGet("CreateAndDownloadPDF")]
+        public async Task<IActionResult> CreateAndDownloadPdf()
+        {
+            // Create memory stream to hold PDF data
+            using (var memoryStream = new MemoryStream())
+            {
+                // Create a new document
+                var document = new Document(PageSize.A4);
+
+                // Set output stream
+                var pdfWriter = PdfWriter.GetInstance(document, memoryStream);
+
+                // Open the document
+                document.Open();
+
+                // Add content, e.g., paragraphs, images, tables
+                // (Replace with your desired content generation logic)
+                Paragraph para = new Paragraph("This is an auto-generated PDF report");
+                document.Add(para);
+
+                // Close the document
+                document.Close();
+
+                // Set response headers for PDF download
+                Response.ContentType = "application/pdf";
+                Response.Headers.Add("Content-Disposition", "attachment; filename=my-report.pdf");
+
+                // Write PDF data to a temporary memory stream
+                using (var tempMemoryStream = new MemoryStream(memoryStream.ToArray()))
+                {
+                    // Copy the data from the temporary memory stream to the response body
+                    tempMemoryStream.Seek(0, SeekOrigin.Begin);
+                    await tempMemoryStream.CopyToAsync(Response.Body);
+                }
+
+                // Flush and close response
+                await Response.Body.FlushAsync();
+            }
+
+            return Ok();
+        }
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using ZawHtutTest.Database;
+using IronPdf;
+
+var Renderer = new ChromePdfRenderer();
+// Create a PDF from a URL or local file path
+using var pdf = Renderer.RenderUrlAsPdf("https://www.amazon.com/?tag=hp2-brobookmark-us-20");
+// Export to a file or Stream
+pdf.SaveAs("url.pdf");
 
 var builder = WebApplication.CreateBuilder(args);
 
